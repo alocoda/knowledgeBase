2e82f582306627ff3d76df36fec48b8ab70dbc5f
Kelvin Lee <klee561@my.bcit.ca>
2020-03-10 22:04:26 -0700

    created skeleton

--- app.js
+++ app.js
@@ -1,11 +1,26 @@
+const bodyParser = require('body-parser');
 const express = require('express');
-const port = process.env.PORT || 3000;
+const expressHbs = require('express-handlebars');
+const path = require('path');
+const routes = require('./routes/routes');
 
 const app = express();
+const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-    res.send('<h1>Hello</h1>');
-});
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.engine('hbs', expressHbs({
+    layoutsDir: 'views/layouts',
+    defaultLayout: 'main',
+    extname: 'hbs'
+}));
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+app.use(routes);
 
 app.listen(port, () => {
     console.log(`Listening on port: ${port}`);
