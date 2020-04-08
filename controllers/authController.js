29395dd4fd46ff02d67d8625810c97008b99add9
Kelvin Lee <kahokelvinlee@gmail.com>
2020-04-07 18:44:11 -0700

    edit: login fail msg

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,7 +2,7 @@ const mod = require("../models/userData");
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
 
-exports.login = async (req, res, next) => {
+exports.login = async (req, res) => {
     let email = req.body.email;
     let pwd = req.body.pwd;
     let hash = await mod.login(email);
@@ -11,11 +11,11 @@ exports.login = async (req, res, next) => {
         req.session.userEmail = email;
         res.redirect("/home")
     } else {
-        res.send('No user found');
+        res.render("index", {loginfailed: true});
     }
 }
 
-exports.signup = async (req, res, next) => {
+exports.signup = async (req, res) => {
     let salt = await bcrypt.genSalt(saltRounds);
     let hashedPwd = await bcrypt.hash(req.body.pwd, salt);
     let userObj = {
@@ -29,7 +29,7 @@ exports.signup = async (req, res, next) => {
     res.render("signup");
 }
 
-exports.signupdetails = async (req, res, next) => {
+exports.signupdetails = async (req, res) => {
     let userObj = req.session.user;
     userObj.image = req.body.image;
     userObj.details = req.body.details;
--- routes/routes.js
+++ routes/routes.js
@@ -7,6 +7,10 @@ router.get('/', (req, res) => {
     res.render('index');
 });
 
+router.get('/login', (req, res) => {
+    res.redirect('/');
+});
+
 router.post('/login', authController.login);
 
 router.post('/signup', authController.signup);
